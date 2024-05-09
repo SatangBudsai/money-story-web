4412379a19d9b920abe7229e7a6cfc7495cd4636
SatangBudsai <satang_budsai@cmu.ac.th>
2024-05-10 00:18:02 +0700

    update design my port

--- src/pages/my-port/index.tsx
+++ src/pages/my-port/index.tsx
@@ -11,6 +11,7 @@ import DatePicker from '@/components/date-picker'
 import DateMultiplePicker from '@/components/date-multiple-picker'
 import DateRangePicker from '@/components/date-range-picker'
 import Container from '@/components/container'
+import { Icon } from '@iconify/react/dist/iconify.js'
 
 type Props = {}
 
@@ -20,8 +21,42 @@ const Home = (props: Props) => {
       <Container className='mt-10'>
         <h1 className='text-5xl font-semibold text-center'>พอร์ตของฉัน</h1>
         <p className='p-1 text-xl text-center'>จัดสรรสัดส่วนการลงทุน</p>
-        {/* <p className='text-xl text-center'>จัดสรรสัดส่วนการลงทุน</p> */}
-        <Card className='mt-10'>
+        <div className='flex flex-col gap-3 mt-10'>
+          <div className='flex gap-2'>
+            <h2 className='text-2xl font-semibold'>รายการพอร์ตทั้งหมด</h2>
+            <Chip size='lg' color='primary' variant='flat'>
+              1 พอร์ต
+            </Chip>
+          </div>
+          <div className='grid grid-cols-3 gap-5 max-lg:grid-cols-2'>
+            <Card className='border-2' shadow='none'>
+              <CardHeader className='justify-between'>
+                <h2 className='text-xl'>พอร์ตหลัก</h2>
+                <Button color='primary'>รายลละเอียด</Button>
+              </CardHeader>
+              <Divider />
+              <CardBody className='flex flex-row flex-wrap gap-2 text-xl'>
+                <Chip variant='flat' color='success' startContent={<Icon icon='streamline:dollar-coin-solid' className='text-xl' />}>
+
+                  1,000 USD
+                </Chip>
+                <Chip variant='flat' color='warning'>
+                  10 หุ้น
+                </Chip>
+              </CardBody>
+            </Card>
+            <Card className='w-full border-2 border-dashed' shadow='none'>
+              <CardBody className='flex items-center justify-center text-'>
+                <h2 className='text-xl'>สร้างพอร์ตใหม่</h2>
+              </CardBody>
+            </Card>
+            {/* <Card className='w-full border-2 border-dashed' shadow='none'>
+              <CardBody className='flex items-center justify-center'>
+              </CardBody>
+            </Card> */}
+          </div>
+        </div>
+        {/* <Card className='mt-10'>
           <CardHeader className='justify-between'>
             <h2 className='px-10 py-2 text-xl rounded-2xl bg-primary text-primary-foreground'>พอร์ตทั้งหมด</h2>
             <Chip size='lg' color='primary' variant='flat'>
@@ -51,7 +86,7 @@ const Home = (props: Props) => {
               </CardBody>
             </Card>
           </CardFooter>
-        </Card>
+        </Card> */}
       </Container>
     </Fragment>
   )
